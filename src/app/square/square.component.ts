bee97eeee9b0d888db7a4624ffc04b435c124201
Your Name <you@example.com>
2021-04-08 19:18:26 -0400

    some updates

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
     <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
     <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ['button { width: 100%; height: 100%; color: rgb(3, 158, 248); font-size: 5em !important; }']
 })
 export class SquareComponent  {
 
